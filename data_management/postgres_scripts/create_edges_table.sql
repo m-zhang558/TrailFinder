b60e4aee28dc62854df3659877790340f8c56334
zhang <zhangmarvin595@gmail.com>
2025-10-31 18:01:59 -0400

    Created edges table

--- data_management/postgres_scripts/create_edges_table.sql
+++ data_management/postgres_scripts/create_edges_table.sql
@@ -1,4 +1,13 @@
 CREATE TABLE edges (
     osmid SERIAL PRIMARY KEY,
-
+    u integer REFERENCES nodes (osmid),
+    v integer REFERENCES nodes (osmid),
+    CHECK ( u <> v ),
+    highway VARCHAR (255),
+    maxspeed integer,
+    name VARCHAR (255),
+    oneway BOOLEAN DEFAULT FALSE,
+    reversed BOOLEAN,
+    length FLOAT,
+    geom GEOMETRY(LINESTRING, 4326)
 );
