3a4fa029529b43a1d6eeeb125faf5d098125bd1b
zhang <zhangmarvin595@gmail.com>
2025-11-13 12:23:40 -0500

    Updated RegionController to take in a Request and return a Response

--- backend/regionFunctions/regionController.py
+++ backend/regionFunctions/regionController.py
@@ -1,9 +1,21 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Request, Response
 
 from backend.db import supabase
 from data_management import data_extraction
 
-def add_region(lat: float, long: float, radius: float, net_type: str):
+def add_region(request: Request) -> Response:
+    lat = request["lat"]
+    if not lat:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Latitude is required")
+    long = request["long"]
+    if not long:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Longitude is required")
+    radius = request["radius"]
+    if not radius:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Radius is required")
+    net_type = request["net_type"]
+    if not net_type:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Net type is required")
     coords = (lat, long)
     graph = data_extraction.get_graph(coords, radius, net_type)
     nodes, edges = data_extraction.convert_graph(graph)
@@ -17,6 +29,6 @@ def add_region(lat: float, long: float, radius: float, net_type: str):
     try:
         supabase.table("nodes").insert(n_json).execute()
         supabase.table("edges").insert(e_json).execute()
-        return {"ok": True, "details": "Inserted nodes and edges"}
+        return Response(status_code=status.HTTP_201_CREATED)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to add region: {e}")
